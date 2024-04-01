a3052f0c8feee324b710fe9f2642350eccc736ae
Bruno-Goodrich <146766985+Bruno-Goodrich@users.noreply.github.com>
2024-04-01 18:43:49 +0000

    Radnomnesss works yay

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import { useState } from 'react';
 import './App.css'
-import { slots, spin, randomSym} from './slotMachine';
+import { slots, randomSymbol} from './slotMachine';
 
 
 const App = () => {
+const [symbol1, setSymbol1] = useState(randomSymbol())
+
+const spin = () => {
+  setSymbol1(randomSymbol())
+}
 
   return (
     <main>
@@ -10,9 +16,9 @@ const App = () => {
           -- you can remove this once you've updated this file
        */}
       Hello World
-      <h1>{slots[5]}</h1>
-      <button onClick={spin}>Spin</button>
-      <h1>{spin}</h1>
+      <h1>BWOONO & LOOSHIN</h1>
+     <button onClick={spin}>Spiny</button>
+     <div>{symbol1}</div>
     </main>
   )
 }
--- src/slotMachine.jsx
+++ src/slotMachine.jsx
@@ -1,10 +1,70 @@
 export let slots = ["seven", "cherries", "cherries", "cherries", "bannana", "bannana", "bannana", "bannana", "bannana", "bannana", "plum", "plum", "plum", "plum", "plum", "plum", "lemon", "lemon", "lemon", "lemon", "lemon", "watermelon", "watermelon", "watermelon", "watermelon", "watermelon", "bell", "bell", "bell", "bell", "orange", "orange", "orange", "orange", "orange", "orange", "bars", "bars"];
 
-export let randomSym = slots[0]
 
-export const spin = () =>{
-  let randomInt= Math.floor(Math.random()*slots.length)
-  return slots[randomInt]
+export const randomSymbol = () =>{
+  let randomInt = Math.floor(Math.random() * slots.length)
+  let randomized = slots[randomInt]
+  return randomized
+}
+/*export const SlotMachine = () =>{
+ let randomSlot = 0
+ 
+ const spin = ()=>{
+   const randomSlot = Math.floor(Math.random * slots.length)
+   return randomSlot
+ }
+  
+ return <div>
+   <button onClick={spin()}>Spin</button>
+    <h1>{slots[randomSlot]}</h1>
+  </div>
+}*/
+const getValueOfSymbol = (symbol) =>{
+  if(symbol == "seven"){
+    value = 100
+  }
+  else if(symbol == "cherries"){
+    value = 80
+  }
+  else if(symbol == "bannana"){
+    value = 50
+  }
+  else if(symbol == "plum"){
+    value = 50
+  }
+  else if(symbol == "lemon"){
+    value = 60
+  }
+  else if(symbol == "watermelon"){
+    value = 60
+  }
+  else if(symbol == "bell"){
+    value = 70
+  }
+  else if(symbol == "orange"){
+    value = 50
+  }
+  else if(symbol == "bars"){
+    value = 90
+  }
+  return value
 }
 
-
+const winnings = (symbol1, symbol2, symbol3) =>{
+  if(symbol1 === symbol2 === symbol3){
+    let money = getValueOfSymbol(symbol1) * 3
+  }
+  else if(symbol1 == symbol2){
+    let money = getValueOfSymbol(symbol1) + getValueOfSymbol(symbol2)
+  }
+  else if(symbol1 == symbol3){
+    let money = getValueOfSymbol(symbol1) + getValueOfSymbol(symbol3)
+  }
+  else if(symbol3 == symbol2){
+    let money = getValueOfSymbol(symbol3) + getValueOfSymbol(symbol2)
+  }
+  else{
+    let money = 0
+  }
+  return money
+}
