612275ddc724f1f3d53cd91dbb1fb83fb5e66e92
Bruno-Goodrich <146766985+Bruno-Goodrich@users.noreply.github.com>
2024-04-05 15:49:10 +0000

    spruced it up a little

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,8 @@ const App = () => {
 const [symbol1, setSymbol1] = useState(randomSymbol())
 const [symbol2, setSymbol2] = useState(randomSymbol())
 const [symbol3, setSymbol3] = useState(randomSymbol())
-const [total, setTotal] = useState(0)
+const [total, setTotal] = useState(1000)
 const [dollar, setDollar] = useState(0)
-
 
 
 const spin = () => {
@@ -18,10 +17,14 @@ const spin = () => {
   let s3 = (randomSymbol())
   let d = winnings(s1,s2,s3)
   setDollar(d)
-  setTotal(total + dollar)
+  setTotal(total - 50 + dollar)
   setSymbol1(s1)
   setSymbol2(s2)
   setSymbol3(s3)
+}
+
+if (total <= 0){
+  setTotal(1000)
 }
   return (
     <main>
@@ -29,12 +32,15 @@ const spin = () => {
           -- you can remove this once you've updated this file
        */}
       
-      <h1>BWOONO's & LOOSHIN's great gambling machine that does not take money (yet).</h1>
-     <button onClick={spin}>Spiny</button>
-     <div>{symbol1}</div>
-     <div>{symbol2}</div>
-     <div>{symbol3}</div>
-     <div>{total}</div>
+      <h1>Welcome to our slot machine that doesn't take real money. Click spiny to spin, doent lose all your money.</h1>
+     <button onClick={spin}>Spin, -$50</button><br></br>
+     <div class ="symbolz">
+      <div class ="sym">{symbol1}</div>
+      <div class ="sym">{symbol2}</div>
+      <div class ="sym">{symbol3}</div>
+     </div>
+     <div>you got ${dollar}</div>
+     <div>${total + dollar}</div>
     </main>
   )
 }
